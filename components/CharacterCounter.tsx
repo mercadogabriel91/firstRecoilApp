c163d0d3f37d0057c3ccf8eabbeb5d4c22ef7aa3
Gabriel <mercado.gabriel91@hotmail.com>
2020-11-06 16:48:20 -0300

    Chore: Simple char counter component

--- components/CharacterCounter.tsx
+++ components/CharacterCounter.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
 import {
-    SafeAreaView,
     StyleSheet,
-    ScrollView,
     View,
     Text,
-    StatusBar,
     TextInput,
 } from 'react-native';
 
@@ -36,34 +33,77 @@ const charCountState = selector({
 });
 
 function CharacterCount() {
+    //Note: 'useRecoilValue' is only used for reading, not writing.
     const count = useRecoilValue(charCountState);
-
-    // return <>Character Count: {count}</>;
-    return <Text>haracter Count: {count}</Text>
+    return (
+        <View style={styles.CharacterCountcontainer}>
+            <Text style={styles.CharacterCountText}>Character Count: {count}</Text>
+        </View>
+    )
 }
 
 function TextInputField() {
+    //Note: 'useRecoilState' is used for writing with hook.
     const [text, setText] = useRecoilState(textState);
 
-    const onChange = (event: any) => {
-        setText(event.target.value);
+    const onChange = (e: any) => {
+        setText(e);
     };
 
     return (
         <View>
-            <TextInput value={text} onChange={onChange}></TextInput>
-            <Text>
-                Echo: {text}
-            </Text>
+            <TextInput style={styles.textInputContainer} value={text} onChangeText={onChange}></TextInput>
+            <View style={{ backgroundColor: '#adb5bd' }}>
+                <Text style={styles.echoContaier}>
+                    Echo: {text}
+                </Text>
+            </View>
         </View>
     );
 }
 
+// COMPONENT
 export default function CharacterCounter() {
     return (
-        <View>
-            <TextInputField />
-            <CharacterCount />
+        <View style={{ backgroundColor: '#ced4da' }}>
+            <View style={{ display: 'flex', alignItems: 'stretch', flexDirection: 'column', marginTop:'20%' }}>
+                <TextInputField />
+                <CharacterCount />
+            </View>
         </View>
     );
 }
+
+// STYLES
+const styles = StyleSheet.create({
+    textInputContainer: {
+        display: "flex",
+        alignContent: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        backgroundColor: '#343a40',
+        borderColor: 'black',
+        borderWidth: 2
+    },
+    echoContaier: {
+        display: 'flex',
+        fontSize: 30,
+        backgroundColor: '#adb5bd',
+        margin: 10,
+        justifyContent: 'center',
+        textAlign: 'center',
+        borderColor: '#adb5bd',
+    },
+    CharacterCountcontainer: {
+        display: 'flex',
+        backgroundColor: '#ced4da',
+        height: '100%'
+    },
+    CharacterCountText: {
+        display: 'flex',
+        fontSize: 20,
+        justifyContent: 'center',
+        flexDirection: 'row',
+        textAlign: 'center'
+    },
+})
